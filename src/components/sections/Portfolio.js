de21d93f4c3ecac788d76f4c541ac53cb6ec0487
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-03-09 13:27:04 +0300

    Added Portfolio section

---
+++ src/components/sections/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const Portfolio = () => {
+  return (
+    <div>
+      <Typography>hi</Typography>
+    </div>
+  );
+};
+
+export default Portfolio;
