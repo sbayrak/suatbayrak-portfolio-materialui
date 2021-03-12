6c36a957af181ab713cda63476afb31bc409eeb6
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-03-12 15:04:01 +0300

    Working on Netlify form 2

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 import {
   CssBaseline,
   Container,
