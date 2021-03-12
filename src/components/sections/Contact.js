596cbbbcd182813be6e811bb7fd5cc5db821d4f7
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-03-12 10:34:30 +0300

    Working on Contact section

---
+++ src/components/sections/Contact.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+import { CssBaseline } from '@material-ui/core';
+
+const Contact = () => {
+  return (
+    <Fragment>
+      <CssBaseline></CssBaseline>
+      <form noValidate autoComplete='off'></form>
+    </Fragment>
+  );
+};
+
+export default Contact;
